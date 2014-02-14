db5e61473853b184e3c7b55db7d28d5e4a126252
Kacey Coughlin <kaceycoughlin@mac.com>
2014-02-14 13:29:20 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="">
+
+    <title>Tweepyness! Score your positivity!</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../js/html5shiv.js"></script>
+      <script src="../js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+    <div class="row">
+    	<div class="col-lg-1"></div>
+    	<div class="col-lg-10">
+    		<h1 class="header text-center">Tweepyness</h1>
+    	</div>
+    	<div class="col-lg-1"></div>
+    </div>
+    <div class="row">
+    	<div class="col-md-8 box index-box">
+    		<h2 class="text-center">Score your Positivity!</h2>
+	      <form class="form-signin" method="post" action="tweets.php">
+	        <p>
+	        <input type="text" name="screenname" class="form-control" placeholder="@johndoe" autofocus>
+	        </p>
+	        <p>
+	        <button class="btn btn-lg btn-primary btn-block" type="submit">Get Tweets!</button>
+	        </p>
+	      </form>
+    	</div>
+    </div>
+    <div class="row">
+      	<h2 class="inbetween text-center">What it is</h2>
+      	<div class="col-md-10 box lead">
+      		<p>
+      			We could all do with a little less negativity in our lives, but how can you find out if you're someone's ray of sunshine, or a storm cloud bringing everyone down? With Tweepyness!
+      		</p><p>
+Tweepyness looks at a persons most recent tweets and produces a score based on the positivity, or negativity, of those tweets! Just enter in any Twitter handle, and Tweepyness will do all the work!
+      		</p><p>
+And if you happen to be a bucket of happiness, Tweepyness will give you ways to monetize on that positivity!
+      		</p>
+      	</div>
+      </div><!-- .row -->
+      
+    	<div class="row">
+    		<h2 class="inbetween text-center">How it works</h2>
+    	<div class="col-lg-1"></div>
+        <div class="col-lg-3 box2 text-center">
+          <img src="img/twitter.png">
+          
+          <p><h4>Enter a <a href="http://www.twitter.com">Twitter</a> handle.</h4></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-3 box2 text-center">
+          <img src="img/smile.png">
+          <p><h4>Get a Tweepyness score.</h4></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-3 box2 text-center">
+          <img src="img/money.png">
+          <p><h4>Monetize with <a href="http://millennialmedia.com">Millennialmedia</a>!</h4></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-1"></div>
+      </div><!-- /.row -->
+      
+      <div class="footer row">
+      	<p class="text-center">
+      		A <a href="//www.kaceycoughlin.com" title="Kacey Coughlin Web Design and Development">Kacey Coughlin</a> joint.
+      	</p>
+      </div>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+    <script type="text/javascript" src="../js/bootstrap.js"></script>
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+    </script>
+  </body>
+</html>
